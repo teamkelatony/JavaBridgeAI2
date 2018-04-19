b64d37161f84dc8f9ee5c86ec870e02ae5d383c2
Thomas Oropeza <thomasoropeza@gmail.com>
2018-04-19 13:34:32 -0700

    modified scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 ant -f $(pwd)/appinventor/appengine CopyBlocklyToBuildWar
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
-dev_appserver.sh --port=8888 --address=0.0.0.0 appinventor/appengine/build/war/
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
+dev_appserver.sh --port=8888 --address=0.0.0.0 appinventor/appengine/build/war/
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
